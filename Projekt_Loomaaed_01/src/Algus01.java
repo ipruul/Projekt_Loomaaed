8beb88ee5c833db09b624f8ad9548f5f0bee7748
Indrek Pruul <indrek.pruul@itcollege.ee>
2015-10-28 21:00:08 +0200

    commit projekti

---
+++ Projekt_Loomaaed_01/src/Algus01.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+
+/**
+ * Created by ipruu_000 on 28.10.2015.
+ */
+public class Algus01 {
+    public  static  void main(string[] args) {
+        System.out.println("Hello");
+    }
+}
